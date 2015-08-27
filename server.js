b39cc6872ac83a7d7b09c80b8749bd95a92210a4
Jesse <jessetbenton@gmail.com>
2015-08-27 00:44:33 -0600

    attempting to get heroku working... last try

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
-app.use(express.logger('dev'));
-
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public')); 
 
